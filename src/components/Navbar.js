495358514d0ba19b4dbc20958ab05e6ba8443e20
impritamk <im.pritamk@gmail.com>
2025-12-06 11:11:38 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,19 +14,25 @@ export default function Navbar() {
     <div style={{
       display: "flex",
       justifyContent: "space-between",
+      alignItems: "center",
       padding: "10px 20px",
       borderBottom: "1px solid #ddd",
-      background: "#fff"
+      background: "#fff",
+      position: "sticky",
+      top: 0,
+      zIndex: 1000
     }}>
-      <strong>Alumni Network</strong>
-
+      <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+        <strong>Alumni Network</strong>
+      </Link>
+      
       <div style={{ display: "flex", gap: 15, alignItems: "center" }}>
-        <Link to="/alumni">Alumni</Link>
-        <Link to="/messages">Messages</Link>
-        <Link to="/jobs">Jobs</Link>
-        <Link to="/profile/edit">Profile</Link>
-
-        <span>Hi, {user?.first_name}</span>
+        <Link to="/" className="text-blue">Home</Link>
+        <Link to="/alumni" className="text-blue">Alumni</Link>
+        <Link to="/messages" className="text-blue">Messages</Link>
+        <Link to="/jobs" className="text-blue">Jobs</Link>
+        <Link to="/profile/edit" className="text-blue">Profile</Link>
+        <span style={{ color: "#6b7280" }}>Hi, {user?.first_name}</span>
         <button className="btn-danger" onClick={doLogout}>Logout</button>
       </div>
     </div>
